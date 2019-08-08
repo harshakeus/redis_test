6fe745c43194da17589f343189ff19c9c33ff06e
harshakeus <p.harsha@keus.in>
2019-08-08 18:32:43 +0530

    redis setup 1

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 var rootpath = require('app-root-path');
 const dbService = require(rootpath+'/db_utils/db_service');
 
+dbService.insert('harsh','abc').then(function(result){console.log(result)}).catch(function(err){console.log('err',err)});
+
 
 setInterval(function(){
     
